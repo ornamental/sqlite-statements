3ecca8925516334c957527b4554f3021e6335eff
ornamental <ornamental.github@gmail.com>
2018-07-14 23:59:01 +0300

    [#14] Add commonly used type name constants

---
+++ src/main/java/dev/ornamental/sqlite/statement/Types.java
@@ -0,0 +1,33 @@
+package dev.ornamental.sqlite.statement;
+
+/**
+ * This class contains some standard type names.
+ */
+public final class Types {
+
+	public static final String INT = "INT";
+
+	public static final String INTEGER = "INTEGER";
+
+	public static final String FLOAT = "FLOAT";
+
+	public static final String REAL = "REAL";
+
+	public static final String BIGINT = "BIGINT";
+
+	public static final String DOUBLE_PRECISION = "DOUBLE PRECISION";
+
+	public static final String TIMESTAMP = "TIMESTAMP";
+
+	public static final String TEXT = "TEXT";
+
+	public static final String VARCHAR = "VARCHAR";
+
+	public static final String BIT = "BIT";
+
+	public static final String BLOB = "BLOB";
+
+	public static final String VARBINARY = "VARBINARY";
+
+	private Types() { }
+}
